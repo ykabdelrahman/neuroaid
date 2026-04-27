3b1d10c156dec51459c9f50bbb894b6a63bc9e7a
Basel Embaby <engbasel2025@gmail.com>
2026-04-28 01:59:59 +0300

    feat: add desktop_webview_window plugin and upgrade dependencies

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,12 +3,14 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  desktop_webview_window
   file_selector_windows
   url_launcher_windows
   window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
